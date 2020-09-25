7bc2d16c3b19614eeaab832d3f92361103a64b73
dspikker <daanspikker@hotmail.com>
2020-09-25 13:42:13 +0200

    firstupload_mainfile

---
+++ db_basics.py
@@ -0,0 +1,44 @@
+import mysql.connector
+import tkinter as tk
+import requests
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  database = "easy_db",
+  user="root",
+  password="Arena11.0!"
+)
+
+sql_select_Query = "SELECT * from cars "
+
+cursor = mydb.cursor()
+cursor.execute(sql_select_Query)
+records = cursor.fetchall()
+
+#receive = requests.get('https://pokeapi.co/api/v2/generation/generation-ii')
+receive = requests.get('https://official-joke-api.appspot.com/random_ten')
+
+print(type(records))
+data = receive.json()
+print(type(data))
+
+for x in data:
+  print(data[x])
+
+
+print(data[2]["punchline"])
+#print(records["name"])
+
+
+for x in records:
+  print(x)
+
+
+
+
+
+
+
+
+
